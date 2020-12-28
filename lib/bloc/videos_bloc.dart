a521908f497a63c0a257c9bf8cfe84a6c222f3b4
Andrey Dershevich <74047963+dersh-ego@users.noreply.github.com>
2020-12-28 08:52:24 +0300

    Update videos_bloc.dart

--- lib/bloc/videos_bloc.dart
+++ lib/bloc/videos_bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:bubu_playlist/models/video_list.dart';
-import 'package:bubu_playlist/videos_service.dart';
+import 'package:bubu_playlist/videos_service.dart'; //bubu_playlist - почему bubu? Разве здесь есть логика привязанная к проекту?
 import 'package:equatable/equatable.dart';
 
 part 'videos_event.dart';
@@ -22,7 +22,7 @@ class VideosBloc extends Bloc<VideosEvent, VideosState> {
       try {
         if (currentState is VideosInitial) {
           final videoList = await videosService.getVideoList(
-              playListId: 'PLpv0AkznBN1frh6QzauYaVa9PL6P7UJK2');
+              playListId: 'PLpv0AkznBN1frh6QzauYaVa9PL6P7UJK2'); //Почему не параметр или константа?
           yield VideosSuccess(videosList: videoList, videos: videoList.videos);
         }
 
@@ -35,12 +35,12 @@ class VideosBloc extends Bloc<VideosEvent, VideosState> {
             query = event.query;
           }
           final videosList = await videosService.getVideoList(
-              playListId: 'PLpv0AkznBN1frh6QzauYaVa9PL6P7UJK2',
+              playListId: 'PLpv0AkznBN1frh6QzauYaVa9PL6P7UJK2', //Почему не параметр или константа?
               pageToken: currentState.videosList.nextPageToken);
           yield VideosSuccess(
               videosList: videosList,
               videos: currentState.videos +
-                  (query == ''
+                  (query == '' //Очень громоздко. Почему не написать условие в разделе where? query == '' || element.video.title.contains(query)
                       ? videosList.videos
                       : videosList.videos
                           .where(
